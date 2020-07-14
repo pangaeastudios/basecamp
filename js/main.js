feb92be0f6119f02c8438c6a3be701f500eeb01f
bitemybit <55141722+bitemybit@users.noreply.github.com>
2020-07-14 22:41:45 +0200

    Basecamp overhaul.

--- js/main.js
+++ js/main.js
@@ -6,3 +6,30 @@ $(document).ready(function(){
     el1.style.backgroundPositionY = -e.offsetY/15 + "px";
   });
 });
+
+var disableE = false;
+
+$("#s-letter").on("mouseover", 
+function(){
+  console.log('e');
+  disableE = true;
+});
+
+$(function() {
+  if(!disableE)
+  {
+    var canvas = document.createElement("canvas");
+    canvas.width = 24;
+    canvas.height = 24;
+    //document.body.appendChild(canvas);
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#000000";
+    ctx.font = "15px FontAwesome";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "middle";
+    ctx.fillText("\uf002", -10, 0);
+    var dataURL = canvas.toDataURL('image/png')
+    $('body').css('cursor', 'url("img/cursor.png"), auto');
+  }
+});
+
